a8a9de17a3da8fec75fa638b7aa490818cf62919
BenGregory23 <bengregorygbr@gmail.com>
2023-01-17 16:02:40 +0100

    ffirst commit

---
+++ src/main/java/com/example/TP3/Tp3AppEngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.TP3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3AppEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3AppEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TP3/Tp3AppEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TP3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp3AppEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
